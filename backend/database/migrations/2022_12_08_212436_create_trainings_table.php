a996f8835e2227812097f41296a648ea112ca0db
Valentinas Kasteckis <41127259+kasteckis@users.noreply.github.com>
2022-12-08 23:52:00 +0200

    Added some new models, removed api landing page

---
+++ backend/database/migrations/2022_12_08_212436_create_trainings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->boolean('owner_is_guest');
+            $table->string('guest_code')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('api')->get('test', function () {
+Route::middleware('auth')->get('test', function () {
     return 'test back + ' . auth()->user()->id;
 });
 
