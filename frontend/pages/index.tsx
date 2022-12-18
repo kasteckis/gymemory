1cbce1ebaafdba013b82cbfc73a0807eac1f7288
kasteckis <valentinas@kasteckis.lt>
2022-12-18 19:29:42 +0200

    Update index page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Container maxWidth="md">
         <h1 style={{textAlign: 'center'}}>GyMemory - coming soon!!</h1>
-        <Link href={'/login'}><h2 style={{textAlign: 'center'}}>Try DEMO. Only guest account login is working now.</h2></Link>
+        <Link href={'/login'}><h2 style={{textAlign: 'center'}}>Try DEMO. Almost everything works.</h2></Link>
     </Container>
   )
 }
