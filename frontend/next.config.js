dda73d6bad39adb088307411f39e45d64521061c
Valentinas Kasteckis <41127259+kasteckis@users.noreply.github.com>
2022-12-27 23:24:59 +0200

    Instantly redirect / to /login

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,15 @@ const withPWA = require("next-pwa")({
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 })
 
 module.exports = nextConfig
