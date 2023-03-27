fed0056af001b5765f004d29d949aca2deb1f759
kasteckis <valentinas@kasteckis.lt>
2023-03-27 20:53:24 +0300

    Ability to generate API token and finish workout with it

--- backend/app/Http/Controllers/SettingsController.php
+++ backend/app/Http/Controllers/SettingsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
+use App\Models\User;
+use App\Models\Workout;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -9,11 +12,50 @@ class SettingsController extends Controller
 {
     public function createOrGetApiToStopWorkout(Request $request): JsonResponse
     {
-        return response()->json('soon todo');
+        $apiToken = '';
+
+        // TODO: Regenerate API key on separate endpoint.
+        // TODO: Check if API key doesnt exist in DB.
+
+        if (auth()->user()) {
+            $user = User::findOrFail(auth()->user()->id);
+
+            if ($user->api_token) {
+                $apiToken = $user->api_token;
+            } else {
+                $apiToken = bin2hex(random_bytes(32));
+
+                $user->api_token = $apiToken;
+                $user->saveOrFail();
+            }
+        } else if (isset(request()->query()['guest-code'])) {
+            $user = User::where('guest_code', request()->query()['guest-code'])->firstOrFail();
+
+            if (!$user->api_token) {
+                $apiToken = bin2hex(random_bytes(32));
+
+                $user->api_token = $apiToken;
+                $user->saveOrFail();
+            }
+        }
+
+        return response()->json($apiToken);
     }
 
     public function stopAllOngoingWorkouts(Request $request): JsonResponse
     {
-        return response()->json('todo');
+        if (isset(request()->query()['token'])) {
+            $user = User::where('api_token', request()->query()['token'])->firstOrFail();
+
+            $workouts = Workout::where('user_id', $user->id)->where('end_date_time', null)->get();
+
+            foreach ($workouts as $workout) {
+                $workout->update(['end_date_time' => new \DateTime()]);
+                $training_id = $workout->training_id;
+                Exercise::where('training_id', $training_id)->update(['completed' => true]);
+            }
+        }
+
+        return response()->json('success if API key exists');
     }
 }
